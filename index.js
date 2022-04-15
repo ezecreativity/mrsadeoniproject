bc9741b16c7bdf8ca8a7f3c283d8d7d88a34d36f
Ezeigbo Emmanuel <76539727+ezecreativity@users.noreply.github.com>
2022-04-15 21:36:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let password = "";
+let oldPassword = "sati";
+password = prompt("Enter your password: ");
+if(password === oldPassword){
+    alert("correct password, click ok to enter");
+}else{
+    window.location = "https://mrsadeoni.netlify.app";
+}
+
+
 let button = document.getElementById("hamburger");
 let navUL = document.getElementById("nav_ul");
 let icon = document.querySelector(".fas fa-bars");
